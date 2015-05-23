01f4352f97a6b264130d705d5b49c95d0ebb6a1e
Isuri Upeksha <upekshais@gmail.com>
2015-05-23 21:52:56 +0530

    Removed Contributor and Volunteer models (All role and user management handel by User)

--- PLTcode/app/models/site.rb
+++ PLTcode/app/models/site.rb
@@ -12,7 +12,9 @@
 class Site < ActiveRecord::Base
   default_scope -> { order('created_at DESC') }
   belongs_to :installation
-  has_many :volunteers , dependent: :destroy
-  has_many :contributors , dependent: :destroy
+
+  #has_many :volunteers , dependent: :destroy
+  #has_many :contributors , dependent: :destroy
+  
   validates :name, presence: true
 end
