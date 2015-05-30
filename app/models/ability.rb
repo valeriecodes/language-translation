d204c30ddcfa62eb70d957f11d5a4bb79a6fe3fc
Isuri Upeksha <upekshais@gmail.com>
2015-05-30 16:37:51 +0530

    volunteer user can only edit his profile and others user profile can be viewed

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,9 +3,9 @@ class Ability
 
   def initialize(user)
     # Define abilities for the passed in user here. For example:
-    user ||= User.new # guest user (not logged in)
+    @user = (user ||= User.new) # guest user (not logged in)
 
-    send(user.role)
+    send(@user.role)
   end
 
   private
@@ -15,8 +15,10 @@ class Ability
 
   def volunteer
     can [:read, :update], Site
-    can :read, [Language, User]
-    can :manage, [Contributor, Article]
+    can :read, [Language]
+    can :manage, [Article]
+    can :read, User, role_id: [2, 3, 4]
+    can :manage, User, role_id: @user.role_id
   end
 
   def contributor
