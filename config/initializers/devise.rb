d149e187151d11ea27c3f38d06a6c5057f7d2aea
Isuri Upeksha <upekshais@gmail.com>
2015-06-03 21:50:19 +0530

    Config additional keys for invite form

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -118,7 +118,7 @@ Devise.setup do |config|
   # The key to be used to check existing users when sending an invitation
   # and the regexp used to test it when validate_on_invite is not set.
   # config.invite_key = {:email => /\A[^@]+@[^@]+\z/}
-  # config.invite_key = {:email => /\A[^@]+@[^@]+\z/, :username => nil}
+   config.invite_key = {email: /\A[^@]+@[^@]+\z/, first_name: '', last_name: '', role_id: ''}
 
   # Flag that force a record to be valid before being actually invited
   # Default: false
