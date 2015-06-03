850baab1bbde503bd7c1830d0d106c48effda39d
Isuri Upeksha <upekshais@gmail.com>
2015-06-03 12:34:22 +0530

    Controller Test for volunteers

--- test/controllers/volunteers_controller_test.rb
+++ test/controllers/volunteers_controller_test.rb
@@ -1,21 +1,40 @@
 require 'test_helper'
 
 class VolunteersControllerTest < ActionController::TestCase
+  tests UsersController
+
   include Devise::TestHelpers
+
   fixtures :all
+
   setup do
-    sign_in users(:one)
-    @user = users(:one)
+    @user = users(:two)
+    sign_in @user
   end
 
-  test "the truth" do
-     assert true
+  def teardown
+    sign_out(@user)
+
+    User.delete_all
   end
 
-  test "no route like /volunteers exist" do
-	assert_raises(ActionController::UrlGenerationError) do
-   	   get :index
-	end
+  #test "the truth" do
+  #  assert true
+  #end
+
+ #test "no route like /volunteers exist" do
+ #  assert_raises(ActionController::UrlGenerationError) do
+ #    get :index
+ #  end
+ #end
+
+  test "Volunteers can view users list #index" do
+    get :index
+
+    assert_template :index
+    
+    puts User.all.map(&:role_id).to_json
+    assert_equal 2, assigns(:users).count, ""
   end
 
 end
