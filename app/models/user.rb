88459715f0da874436a15e19fe3f89deeb40578e
Isuri Upeksha <upekshais@gmail.com>
2015-06-15 09:10:34 +0530

    Added addition input fields into send invitation & accept invitation form

--- app/models/user.rb
+++ app/models/user.rb
@@ -37,7 +37,7 @@ class User < ActiveRecord::Base
   default_scope -> { order('created_at DESC') }
   validates_uniqueness_of :username
   validates_confirmation_of :password, length: { in: 6..20 }
-  validates_presence_of :username, :role, :login_approval, :first_name, :last_name
+  validates_presence_of :username, :role_id, :first_name
 
   default_value_for :role_id, 5
 
