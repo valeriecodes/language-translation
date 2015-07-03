a694d7a41e61e2ebcdbcf73c8a02f9becc01f162
Won Wook SONG <wsong0512@gmail.com>
2015-07-04 01:39:34 +0900

    42-fix errors that didn't render the view and resourcify Sites for Rolify

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,50 +1,52 @@
 class SitesController < ApplicationController
- load_and_authorize_resource
+  load_and_authorize_resource
 
- def new
-  @site = Site.new
- end
-
- def index
-  @sites = Site.all
- end
-
- def create
-  @site = Site.new(site_params)
- 
-  if @site.save
-   redirect_to @site
-  else
-   render 'new'
+  def new
+    @site = Site.new
   end
- end
 
- def edit
-  @site = Site.find(params[:id])
- end
-
- def destroy
-  @site = Site.find(params[:id])
-  @site.destroy
- 
-  redirect_to sites_path
- end
-
- def show
-  @site = Site.find(params[:id])
- end
-
- def update
-  @site = Site.find(params[:id])
- 
-  if @site.update(site_params)
-    redirect_to @site
-  else
-    render 'edit'
+  def index
+    @sites = Site.all
   end
- end
- 
- private
+
+  def create
+    @site = Site.new(site_params)
+
+    if @site.save
+      redirect_to @site
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @site = Site.find(params[:id])
+  end
+
+  def destroy
+    @site = Site.find(params[:id])
+    @site.destroy
+
+    redirect_to sites_path
+  end
+
+  def show
+    @site = Site.find(params[:id])
+    @volunteers = User.with_role :volunteer, @site
+    @contributors = User.with_role :contributor, @site
+  end
+
+  def update
+    @site = Site.find(params[:id])
+
+    if @site.update(site_params)
+      redirect_to @site
+    else
+      render 'edit'
+    end
+  end
+
+  private
   def site_params
     params.require(:site).permit(:name, :installation_id)
   end
