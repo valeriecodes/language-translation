09c4cb5acd264a7865c448963451448cbff57d92
Won Wook SONG <wsong0512@gmail.com>
2015-07-25 21:04:06 +0900

    Reconfigure Navigation and Ability correctly

--- config/navigation.rb
+++ config/navigation.rb
@@ -4,32 +4,44 @@ SimpleNavigation::Configuration.run do |navigation|
 
   # Define the primary navigation
   navigation.items do |primary|
-    primary.item :users, 'Members', users_path, highlights_on: :subpath if current_user and current_user.has_any_role? :admin, :volunteer
-    
-    primary.item :posts, "Posts", installations_path, highlights_on: :subpath do |sub|
-      sub.dom_class = 'nav nav-pills'
-      sub.item :photos, 'New post', new_installation_path
-    end  if current_user and current_user.has_role? :admin
+    if current_user and current_user.has_any_role? :superadmin, :admin, {name: :volunteer, resource: :any}
+      primary.item :users, 'Members', users_path, highlights_on: :subpath
+    end
 
-    primary.item :sites, "Sites", sites_path, highlights_on: :subpath do |sub|
-      sub.dom_class = 'nav nav-pills'
-      sub.item :photos, 'New site', new_site_path
-    end  if current_user and current_user.has_any_role? :admin, :volunteer
+    if current_user and current_user.has_any_role? :superadmin, :admin
+      primary.item :posts, "Posts", installations_path, highlights_on: :subpath do |sub|
+        sub.dom_class = 'nav nav-pills'
+        sub.item :photos, 'New post', new_installation_path
+      end
+    end
 
-    primary.item :languages, "Languages", languages_path, highlights_on: :subpath do |sub|
-      sub.dom_class = 'nav nav-pills'
-      sub.item :photos, 'New language', new_language_path
-    end  if current_user and current_user.has_any_role? :admin, :volunteer
+    if current_user and current_user.has_any_role? :superadmin, :admin, {name: :volunteer, resource: :any}
+      primary.item :sites, "Sites", sites_path, highlights_on: :subpath do |sub|
+        sub.dom_class = 'nav nav-pills'
+        sub.item :photos, 'New site', new_site_path
+      end
+    end
 
-    primary.item :category, "Category", categories_path, highlights_on: :subpath do |sub|
-      sub.dom_class = 'nav nav-pills'
-      sub.item :category, 'New Category', new_category_path 
-    end if current_user and current_user.has_any_role? :admin, :volunteer
+    if current_user and current_user.has_any_role? :superadmin, :admin, {name: :volunteer, resource: :any}
+      primary.item :languages, "Languages", languages_path, highlights_on: :subpath do |sub|
+        sub.dom_class = 'nav nav-pills'
+        sub.item :photos, 'New language', new_language_path
+      end
+    end
 
-    primary.item :photos, "Photos", articles_path, highlights_on: :subpath do |sub|
-      sub.dom_class = 'nav nav-pills'
-      sub.item :photos, 'New photo', new_article_path 
-    end  if current_user and current_user.has_any_role? :admin, :volunteer, :contributor
+    if current_user and current_user.has_any_role? :superadmin, :admin, {name: :volunteer, resource: :any}
+      primary.item :category, "Category", categories_path, highlights_on: :subpath do |sub|
+        sub.dom_class = 'nav nav-pills'
+        sub.item :category, 'New Category', new_category_path
+      end
+    end
+
+    if current_user and current_user.has_any_role? :superadmin, :admin, {name: :volunteer, resource: :any}, {name: :contributor, resource: :any}
+      primary.item :photos, "Photos", articles_path, highlights_on: :subpath do |sub|
+        sub.dom_class = 'nav nav-pills'
+        sub.item :photos, 'New photo', new_article_path
+      end
+    end
 
     primary.dom_class = 'nav'
   end
