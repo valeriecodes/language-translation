c946f7b8702f55fe1ea25db842379638760185e5
Won Wook SONG <wsong0512@gmail.com>
2015-08-17 18:56:16 +0900

    80-indentation, comments for config files
    - Also changed Pj1 -> Plt

--- config/navigation.rb
+++ config/navigation.rb
@@ -1,19 +1,21 @@
 # Configures your navigation
 SimpleNavigation::Configuration.run do |navigation|
-   navigation.selected_class = 'active'
+  navigation.selected_class = 'active'
 
   # Define the primary navigation
   navigation.items do |primary|
     primary.dom_class = 'MainMenu sidebarNav'
-    
+
+    # Superadmins, Admins, and Volunteers have access to members. Admins and Volunteers have access to those that belong to their own organization only.
     if current_user and current_user.has_any_role? :superadmin, :admin, {name: :volunteer, resource: :any}
       primary.item :users, 'Members', users_path, highlights_on: :subpath
     end
 
+    # Superadmins, Admins, Volunteers, and Contributors have access to the Articles.
     if current_user and current_user.has_any_role? :superadmin, :admin, {name: :volunteer, resource: :any}, {name: :contributor, resource: :any}
-      primary.item :photos, "Photos", articles_path, highlights_on: :subpath do |sub|
+      primary.item :articles, "Articles", articles_path, highlights_on: :subpath do |sub|
         sub.dom_class = 'nav nav-pills'
-        sub.item :new_photo, 'New photo', new_article_path
+        sub.item :new_article, 'New article', new_article_path
       end
     end
   end
