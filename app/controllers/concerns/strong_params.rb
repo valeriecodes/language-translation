f9577eddad3b8d87d5ad577dab2324f926e57993
Won Wook SONG <wsong0512@gmail.com>
2015-08-18 02:26:30 +0900

    89-Fix to integrate Bootstrap with the new design

--- app/controllers/concerns/strong_params.rb
+++ app/controllers/concerns/strong_params.rb
@@ -2,7 +2,7 @@ module StrongParams
   protected
   def article_params
     params.require(:article).permit(:category_id, :english, :phonetic, :picture, :remove_picture, :language_id).tap do |whitelisted|
-      whitelisted[:language_id] = Language.where(name: current_user.lang).first.try(:id) if current_user.has_any_role? :contributor
+      whitelisted[:language_id] = Language.where(name: current_user.lang).first.try(:id) if current_user.has_role? :contributor, :any
       whitelisted[:state] = params[:article][:state] if can?(:delete, Article)
     end
   end
